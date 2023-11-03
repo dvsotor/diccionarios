d3bd56d517038ce88ac1cfb6203a0d02d0cb2e70
dvsotor <142631389+dvsotor@users.noreply.github.com>
2023-11-03 17:53:15 -0500

    Add files via upload

---
+++ diccionarios.py
@@ -0,0 +1,134 @@
+#Taller: Crear una lista de diccionarios con las siguientes llaves
+#nombre, apellido, promedio, nivel de programación, edad, estado civil
+
+compañero0={
+    "nombre": 'Diana',
+    "apellido": 'Soto',
+    "promedio": 3.6,
+    "nivel_de_programación": 2,
+    "edad": 20,
+    "estado_civil": 'relación'
+}
+
+compañero1={
+    "nombre": 'Alejandro', 
+    "apellido": 'García', 
+    "promedio": 3.78, 
+    "nivel_de_programación": 2,
+    "edad": 21, 
+    "estado_civil": 'soltero',
+}
+
+compañero2={
+    "nombre": 'Juan',
+    "apellido": 'Rivera',
+    "promedio": 3.4,
+    "nivel_de_programación": 3,
+    "edad": 21, 
+    "estado_civil": 'soltero'
+}
+
+compañero3={
+    "nombre": 'Sandra',
+    "apellido": 'Junco',
+    "promedio": 4.39,
+    "nivel_de_programación": 2,
+    "edad": 23,
+    "estado_civil": 'soltero'
+}
+
+compañero4={
+    "nombre": 'Brayan',
+    "apellido": 'Ortiz',
+    "promedio": 3.09,
+    "nivel_de_programación": 2,
+    "edad": 20,
+    "estado_civil": 'soltero'
+}
+
+compañero5={
+    "nombre": 'Jhon',
+    "apellido": 'Neva',
+    "promedio": 3.5,
+    "nivel_de_programación": 2,
+    "edad": 20,
+    "estado_civil": 'soltero'
+}
+
+compañero6={
+    "nombre": 'Luisa',
+    "apellido": 'Betancourt',
+    "promedio": 35,
+    "nivel_de_programación": 3,
+    "edad": 19,
+    "estado_civil": 'soltero'
+}
+
+compañero7={
+    "nombre": 'Linda',
+    "apellido":'Ramos',
+    "promedio": 3.61,
+    "nivel_de_programación": 2,
+    "edad": 25,
+    "estado_civil": 'relación'
+}
+
+compañero8={
+    "nombre": 'Maria',
+    "apellido": 'Pardo',
+    "promedio": 3.8,
+    "nivel_de_programación": 2,
+    "edad": 23,
+    "estado_civil": 'relación'
+}
+
+compañero9={
+    "nombre": 'Johan',
+    "apellido": 'Galindo',
+    "promedio": 3.38,
+    "nivel_de_programación": 2,
+    "edad": 22,
+    "estado_civil": 'soltero'
+}
+
+compañero10={
+    "nombre": 'Ángel',
+    "apellido": 'Molano',
+    "promedio": 3.6,
+    "nivel_de_programación": 3,
+    "edad": 24,
+    "estado_civil": 'relación'
+}
+
+compañero11={
+    "nombre": 'Nikole',
+    "apellido": 'Florez',
+    "promedio": 4.19,
+    "nivel_de_programación": 2.5,
+    "edad": 21,
+    "estado_civil": 'soltero'
+}
+
+compañero12={
+    "nombre": 'Andrés',
+    "apellido": 'Segura',
+    "promedio": 4.1,
+    "nivel_de_programación": 3,
+    "edad": 24,
+    "estado_civil": 'casado'
+}
+
+compañero13={
+    "nombre": 'Ruth',
+    "apellido": 'Durán',
+    "promedio": 4.0,
+    "nivel_de_programación": 3,
+    "edad": 22,
+    "estado_civil": 'unión libre'
+}
+ #Corregir
+compas=[compañero0,compañero1,compañero2,compañero3,compañero4,compañero5,compañero6,compañero7,compañero8,compañero9,compañero10,compañero11,compañero12,compañero13]
+suma=0
+for x in compas:
+    suma+=compas
+    print(suma[compas])
